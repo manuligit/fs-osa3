804c505b846aa4c4655b91def13bc3297e7e3886
manuligit <riina.vaisanen@gmail.com>
2018-02-06 18:30:45 +0200

    t3.3

--- index.js
+++ index.js
@@ -37,6 +37,19 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  console.log(id)
+  const person = persons.find(person => person.id.toString() === id.toString())
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

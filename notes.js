4d43171d57569d78553fb19772bfd5eca1f4b5e1
manuligit <riina.vaisanen@gmail.com>
2018-02-12 22:00:01 +0200

    osa4

--- notes.js
+++ notes.js
@@ -1,117 +1,36 @@
 const express = require('express')
 const app = express()
-const bodyPerser = require('body-parser')
+const bodyParser = require('body-parser')
 const cors = require('cors')
-const Note = require('./models/note')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const notesRouter = require('./controllers/notes')
 
-const logger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then( () => { 
+    console.log('connected to database', process.env.MONGODB_URI) 
+  })
+  .catch( err => { 
+    console.log(err) 
+  })  
+
+mongoose.Promise = global.Promise
 
 app.use(cors())
-app.use(bodyPerser.json())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+app.use(middleware.logger)
 
-app.use(logger)
+app.use('/api/notes', notesRouter)
 
-const formatNote = (note) => {
-  const formattedNote = { ...note._doc, id: note._id }
-  delete formattedNote._id
-  delete formattedNote.__v
+app.use(middleware.error)
 
-  return formattedNote
-}
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-  Note
-    .find({})
-    .then(notes => {
-      response.json(notes)
-    })
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  Note
-    .findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(formatNote(note))
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
-})
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date()
-  })
-
-  note
-    .save()
-    .then(savedNote => {
-      response.json(formatNote(savedNote))
-    })
-})
-
-app.put('/api/notes/:id', (request, response) => {
-  const body = request.body
-
-  const note = {
-    content: body.content,
-    important: body.important
-  }
-
-  Note
-    .findByIdAndUpdate(request.params.id, note, { new: true } )
-    .then(updatedNote => {
-      response.json(formatNote(updatedNote))
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  Note
-    .findByIdAndRemove(request.params.id)
-    .then(response.status(204).end())
-    .catch(error => {
-      console.log(error.name)
-      response.status(400).send({ error: 'malformatted id' })
-    })
-
-})
-
-const error = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(error)
-
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
-
